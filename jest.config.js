38cd8bfd1b5cd0fdd29016b92ddd5b8a790e1b42
hsafari99 <hsafari99@gmail.com>
2025-05-02 09:09:01 -0400

    Ignore integration test

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/integrationTest/'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
